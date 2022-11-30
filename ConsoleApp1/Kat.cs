b93d02ac0892601d6586727f615c06004fbdbe5f
Rune Røddik Hansen <rune9485@elev.tec.dk>
2022-11-30 08:21:21 +0100

    Add project files.

---
+++ ConsoleApp1/Kat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Kat
+    {
+        public string? Farve { get; set; }
+
+        public void ChangColorToYellow()
+        {
+            Farve = "Gul";
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿using ConsoleApp1;
+
+Kat kat= new Kat();
+kat.Farve = "Grå";
+Console.WriteLine(kat.Farve);
+kat.ChangColorToYellow();
+Console.WriteLine(kat.Farve);
